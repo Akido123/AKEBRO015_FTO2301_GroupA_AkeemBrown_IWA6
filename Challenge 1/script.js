07b1bb8e80b057201d0ca2240139e8ee3d410e36
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-04-20 20:06:32 +0200

    Update: completed and corrected challenges 1 and 2

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -11,8 +11,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = NaN == parseInt(primaryPhone)
-const secondaryValid = NaN == parseInt(secondaryPhone)
+const primaryValid = !isNaN(primaryPhone)
+const secondaryValid = !isNaN(secondaryPhone)
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
